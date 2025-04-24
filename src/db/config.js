ca1670541db8d09e4d210f14f394248f99da4013
Pedro Souza <pedro.ads510@gmail.com>
2025-04-24 00:22:51 -0300

    feat: add database connection

---
+++ src/db/config.js
@@ -0,0 +1,22 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = process.env.DATABASE_URI;
+export const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    process.exit(1);
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectToDatabase } from "./db/config.js";
 
 dotenv.config();
 
+await connectToDatabase();
 const app = express();
 
 app.listen(3000, () => {
