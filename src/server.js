267650382b6f49a9df88b9d3a8fc6ee1969f1f5c
Pedro Souza <pedro.ads510@gmail.com>
2025-04-26 18:12:51 -0300

    feat: add product routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { connectToDb } from "./db/config.js";
+import productRoutes from "./routes/product.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("server is running");
+app.use("/products", productRoutes);
+app.use("/categories", categoryRoutes);
+
+connectToDb.then(() => {
+  app.listen(3000, () => {
+    console.log("server is running");
+  });
 });
